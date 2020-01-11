8458a1833905b6eaa2b82312c8d1bd6bd2883fdb
itayp1 <peretz.itay@gmail.com>
2020-01-11 09:24:00 +0200

    heroku uri

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { AsyncStorage } from "react-native";
 
-const url = "http://ca41a880.ngrok.io";
+const url = "https://teacher4u-test.herokuapp.com";
 
 const instance = axios.create({
   baseURL: url
